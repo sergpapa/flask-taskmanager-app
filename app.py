6011163b97f6e608cc128981d8ad32b962204079
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-02-14 15:30:35 +0000

    added basic log in page

--- app.py
+++ app.py
@@ -45,6 +45,22 @@ def register():
         flash("Registration successfull")
     return render_template("register.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+         # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # check if hashed password matches user input
+            if check_password_hash(
+                existing_user["password", request.form.get("password")]):
+                    session["user"] = request.form.get("username").lower()
+                    flash("welcome, {}".format(request.form.get("username")))
+            
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")), debug=True)
