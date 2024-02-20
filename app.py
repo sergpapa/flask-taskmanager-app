0bcc5171eaadc008acdf51f89e15a24cc259cf04
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-02-20 14:51:38 +0000

    added done/edit and edit task for individual tasks

--- app.py
+++ app.py
@@ -109,6 +109,25 @@ def add_task():
     categories = list(mongo.db.categories.find().sort("category_name", 1))
     return render_template("add_task.html", categories=categories)
 
+@app.route("/edit_task/<task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        task = {
+            "category": request.form.get("category_name"),
+            "task_name": request.form.get("task_name"),
+            "task_description": request.form.get("task_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.update_one()
+        flash("Task supdated added")
+        return redirect(url_for("get_tasks"))
+    task = mongo.db.categories.find_one({"_id": ObjectId(task_id)})
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("edit_task.html",task=task, categories=categories)
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")), debug=True)
