911c2d41bb5d82d68664dc71cb2ab87b200613d2
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-02-22 13:46:00 +0000

    update tasks functionality completed

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,19 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-    $('.collapsible').collapsible();
-    $('.tooltipped').tooltip();
-    $('select').formSelect();
-    $('.datepicker').datepicker({
-      format: "dd mmmm, yyyy",
-      yearRange: 3,
-      showClearBtn: true,
-      i18n: {
-        done: "Select"
-    }
+/*
+    jQuery for MaterializeCSS initialization
+*/
+
+$(document).ready(function () {
+    $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+    $("select").formSelect();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
     });
 
     validateMaterializeSelect();
@@ -39,4 +43,4 @@ $(document).ready(function(){
             }
         });
     }
-  });
+});
